baf699d63d8ba22d4ccc602419a9cd70b8e509c3
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-10 23:16:46 +0900

    Fix: header(메뉴 제외), section intro PC 구조 작업

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+let sections = gsap.utils.toArray("section");
+
+gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".main",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: "+=7000",
+        // end: document.querySelector("#parallax__cont").offsetWidth,
+    }
+});
+
+  // a태그 기본동작 방지
+  $(document).on('click', 'a[href="#"]', function (e) {
+    e.preventDefault();
+  });
+
+const globalNav = document.querySelector('.global-nav');
+const globalNavMenu = globalNav.querySelector('.global-nav__menu');
+
+globalNavMenu.addEventListener('click', function() {
+  globalNav.classList.toggle('is-open');
+});

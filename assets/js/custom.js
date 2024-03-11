2bd05dbf972abe0bd62611d14925fc028207b31b
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-11 21:15:22 +0900

    Fix: section-explore > 마크업 추가

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,17 @@
 let sections = gsap.utils.toArray("section");
 
-gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".main",
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-        end: "+=7000",
-        // end: document.querySelector("#parallax__cont").offsetWidth,
-    }
-});
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".main",
+//         pin: true,
+//         scrub: 1,
+//         snap: 1 / (sections.length - 1),
+//         // end: "+=30000",
+//         end: document.querySelector(".main").offsetWidth,
+//     }
+// });
 
   // a태그 기본동작 방지
   $(document).on('click', 'a[href="#"]', function (e) {

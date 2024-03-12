b42eedb36fdc06b86802053171834d4ce97139af
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-12 12:10:01 +0900

    Fix: 네비게이션 작업

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,9 +18,64 @@ let sections = gsap.utils.toArray("section");
     e.preventDefault();
   });
 
-const globalNav = document.querySelector('.global-nav');
-const globalNavMenu = globalNav.querySelector('.global-nav__menu');
+const body = document.querySelector('body');
+const headerEl = document.querySelector('.header');
+let windowWidth = 0;
+let scrollPosition = 0;
 
-globalNavMenu.addEventListener('click', function() {
-  globalNav.classList.toggle('is-open');
+// body fixed
+function enable() {
+  scrollPosition = window.pageYOffset;
+  body.style.overflow = 'hidden';
+  body.style.position = 'fixed';
+  // body.style.top = `-${scrollPosition}px`;
+  body.style.width = '100%';
+}
+
+function disable() {
+  body.style.removeProperty('overflow');
+  body.style.removeProperty('position');
+  body.style.removeProperty('top');
+  body.style.removeProperty('width');
+  // window.scrollTo(0, scrollPosition);
+}
+
+headerEl.addEventListener('click', function(event) {
+  if(event.target.classList.contains('header__menu-btn')) {
+    globalNavMenuBtnToggle();
+  }
 });
+
+window.addEventListener('resize', function() {
+  windowWidth = window.innerWidth;
+
+  if((windowWidth > 991) && (headerEl.classList.contains('is-open'))) {
+    navigationClose();
+  } else {
+    return;
+  }
+});
+
+function globalNavMenuBtnToggle() {
+    if(headerEl.classList.contains('is-open')) {
+      navigationClose();
+    } else {
+      navigationOpen();
+    }
+}
+
+function navigationOpen() {
+  const globalNavMenuBtn = document.querySelector('.header__menu-btn');
+
+  headerEl.classList.add('is-open');
+  // enable();
+  globalNavMenuBtn.setAttribute('aria-label', 'menu close');
+}
+
+function navigationClose() {
+  const globalNavMenuBtn = document.querySelector('.header__menu-btn');
+
+  headerEl.classList.remove('is-open');
+  // disable();
+  globalNavMenuBtn.setAttribute('aria-label', 'menu open');
+}
